c9071f438518969bf9b30a62ab1347a459db785c
toanlebao <toan.lebao@halliburton.com>
2022-01-09 15:34:02 +0700

    initial

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterController : MonoBehaviour
+{
+    public int blood = 5;
+
+    private NavMeshAgent mMeshAgent;
+
+    private Brick mPreviousBrick;
+
+    private Brick mCurrentBrick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                GameObject hitObject = hit.transform.gameObject;
+                Brick brick = hitObject.GetComponent<Brick>();
+                if (brick != null) {
+                    mMeshAgent.SetDestination(hit.transform.position);
+                }
+            }
+        }
+
+        DetectMine();
+    }
+
+    private void DetectMine()
+    {
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+        if (Physics.SphereCast(ray, 0.2f, out hit)) {
+            GameObject hitObject = hit.transform.gameObject;
+            Brick brick = hitObject.GetComponent<Brick>();
+            if (brick != null) {
+                brick.ShowSecret();
+
+                if (brick.mine && mPreviousBrick != null) {
+                    // mMeshAgent.SetDestination(mPreviousBrick.transform.position);
+                    blood -= 1;
+                }
+
+                if (brick != mCurrentBrick) {
+                    mPreviousBrick = mCurrentBrick;
+                    mCurrentBrick = brick;
+                }
+            }
+        }
+    }
+    private void OnMouseEnter()
+	{
+        GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+	}
+	private void OnMouseExit()
+	{
+        GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+	}
+}
